ab766ec40549744bdc14ccc2b0823d1f69268be3
Armin <dead4game@yahoo.com>
2023-08-04 21:20:51 +0330

    add di

---
+++ Tax/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Tax.BusinessLogic;
+using Tax.Models;
+using Tax.Repository;
+
+namespace Tax;
+
+public class Program
+{
+    public static void Main()
+    {
+        //setup our DI
+        new ServiceCollection()
+            .AddLogging()
+            .AddScoped<IRepository<Holiday>, HolidaysRepository>()
+            .AddScoped<IRepository<TimeRangeTaxFee>, TimeRangeTaxFeesRepository>()
+            .AddScoped<IRepository<string>, TollFreeVehiclesRepository>()
+            .AddScoped<ICongestionTaxCalculatorBusinessLogic, CongestionTaxCalculatorBusinessLogic>()
+            .BuildServiceProvider();
+    }
+}
