6254eacdc84b1f497e84d17527150a9b06f67a53
Armin <dead4game@yahoo.com>
2023-08-06 12:56:18 +0330

    extract sample data to its own class

---
+++ Tax.Test/SampleData/VehiclePassDatesSampleData.cs
@@ -0,0 +1,24 @@
+namespace Tax.Test.SampleData;
+
+public static class Vehicle
+{
+    public static List<DateTime> PassDatesSampleData() => new()
+    {
+        DateTime.Parse("2013-01-14 21:00:00"),
+        DateTime.Parse("2013-01-15 21:00:00"),
+        DateTime.Parse("2013-02-07 06:23:27"),
+        DateTime.Parse("2013-02-07 15:27:00"),
+        DateTime.Parse("2013-02-08 06:27:00"),
+        DateTime.Parse("2013-02-08 06:20:27"),
+        DateTime.Parse("2013-02-08 14:35:00"),
+        DateTime.Parse("2013-02-08 15:29:00"),
+        DateTime.Parse("2013-02-08 15:47:00"),
+        DateTime.Parse("2013-02-08 16:01:00"),
+        DateTime.Parse("2013-02-08 16:48:00"),
+        DateTime.Parse("2013-02-08 17:49:00"),
+        DateTime.Parse("2013-02-08 18:29:00"),
+        DateTime.Parse("2013-02-08 18:35:00"),
+        DateTime.Parse("2013-03-26 14:25:00"),
+        DateTime.Parse("2013-03-28 14:07:27")
+    };
+}
--- Tax.Test/UnitTestsCongestionTaxCalculator.cs
+++ Tax.Test/UnitTestsCongestionTaxCalculator.cs
@@ -4,6 +4,7 @@ using Tax.Interfaces;
 using Tax.Models;
 using Tax.Repository;
 using Xunit;
+using Vehicle = Tax.Test.SampleData.Vehicle;
 
 namespace Tax.Test;
 
@@ -38,25 +39,7 @@ public class UnitTestsCongestionTaxCalculator
     public void GetTaxReturnsCorrectFee()
     {
         // arrange
-        var dates = new List<DateTime>
-        {
-            DateTime.Parse("2013-01-14 21:00:00"),
-            DateTime.Parse("2013-01-15 21:00:00"),
-            DateTime.Parse("2013-02-07 06:23:27"),
-            DateTime.Parse("2013-02-07 15:27:00"),
-            DateTime.Parse("2013-02-08 06:27:00"),
-            DateTime.Parse("2013-02-08 06:20:27"),
-            DateTime.Parse("2013-02-08 14:35:00"),
-            DateTime.Parse("2013-02-08 15:29:00"),
-            DateTime.Parse("2013-02-08 15:47:00"),
-            DateTime.Parse("2013-02-08 16:01:00"),
-            DateTime.Parse("2013-02-08 16:48:00"),
-            DateTime.Parse("2013-02-08 17:49:00"),
-            DateTime.Parse("2013-02-08 18:29:00"),
-            DateTime.Parse("2013-02-08 18:35:00"),
-            DateTime.Parse("2013-03-26 14:25:00"),
-            DateTime.Parse("2013-03-28 14:07:27")
-        };
+        var dates = Vehicle.PassDatesSampleData();
 
         IVehicle vehicle = new Car();
 
